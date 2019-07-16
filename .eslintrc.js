ba5ffeefaca9207b168f0d6e9325e9f1508ae974
Raphael Tacla <raphael.tacla@pd.griaule>
2019-07-16 18:14:59 -0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,43 @@
+module.exports = {
+	'env': {
+		'browser': true,
+		'es6': true,
+		'node': true
+	},
+	'extends': [
+		'plugin:@typescript-eslint/recommended',
+		'plugin:react/recommended'
+	],
+	'globals': {
+		'Atomics': 'readonly',
+		'SharedArrayBuffer': 'readonly'
+	},
+	'parser': '@typescript-eslint/parser',
+	'parserOptions': {
+		'ecmaFeatures': {
+			'jsx': true
+		},
+		'ecmaVersion': 2018,
+		'sourceType': 'module'
+	},
+	'plugins': [
+		'react',
+		'@typescript-eslint'
+	],
+	'settings': {
+		'react': {
+			'version': 'detect'
+		}
+	},
+	'rules': {
+		'@typescript-eslint/indent': [
+			'error',
+			'tab'
+		],
+		'@typescript-eslint/semi': [
+			'error',
+			'always'
+		],
+		'react/prop-types': 'off'
+	}
+};
